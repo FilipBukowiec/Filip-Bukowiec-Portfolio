bb2995d5d1f810dbfbbc3f6b2c3321f1953610ab
FilipBukowiec <filip.bukowiec@interia.pl>
2022-07-26 14:36:46 +0200

    first java script

---
+++ script.js
@@ -0,0 +1,70 @@
+
+const Content = document.getElementById("Content");
+const AboutMe = document.getElementById("AboutMe");
+
+const Contact = document.getElementById("Contact");
+
+
+
+const ContentAboutMe = document.getElementById("ContentAboutMe");
+const ContentContact = document.getElementById("ContentContact");
+
+
+
+
+
+AboutMe.addEventListener("click", () => {
+    Content.innerHTML = `
+            <h1>About</h1>
+            <h4>wiekdjkfgdkghfdgfdgfdg</h4>
+            <p>
+            srole
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam a
+              repudiandae molestias, voluptates repellendus eum nemo? Odio,
+              vitae accusantium. Nihil blanditiis fugiat doloremque ipsa.
+              Dolorem accusantium aperiam eaque dolor blanditiis? A asperiores
+              ea eaque aperiam fuga assumenda soluta similique sed! Earum
+              placeat nulla natus accusantium totam corrupti maiores, dolore
+              explicabo eveniet accusamus sed eos nisi blanditiis ratione in
+              doloremque quod? Quidem fugit sint labloreore? Ae porro
+              necessitatibus officia doloribus vero sint molestias accusantium
+              aliquid ipsa tempore maxime assumenda. Dolorum eius quas rem
+              voluptates optio voluptate. Voluptate ut nesciunt veritatis eaque
+              repellat, dolorum rerum officiis ducimus, ipsum assumenda
+              voluptatibus neque quos. Quidem optio veniam nisi dolor fuga,
+              asperiores non praesentium tempore delectus, pariatur dolorem
+              necessitatibus ab. Optio tempore ipsa nulla beatae cumque ad dicta
+              nemo culpa! Iusto consequuntur in sed dicta repellat doloribus
+              placeat perspiciatis tempore, praesentium ratione natus quo
+              consequatur officia, dolorem assumenda vitae necessitatibus!
+            </p>
+            <hr />`
+});
+
+Contact.addEventListener("click", () => {
+    Content.innerHTML = `
+          
+            <h1>Contact</h1>
+            <h4>wiekdjkfgdkghfdgfdgfdg</h4>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam a
+              repudiandae molestias, voluptates repellendus eum nemo? Odio,
+              vitae accusantium. Nihil blanditiis fugiat doloremque ipsa.
+              Dolorem accusantium aperiam eaque dolor blanditiis? A asperiores
+              ea eaque aperiam fuga assumenda soluta similique sed! Earum
+              placeat nulla natus accusantium totam corrupti maiores, dolore
+              explicabo eveniet accusamus sed eos nisi blanditiis ratione in
+              doloremque quod? Quidem fugit sint labloreore? Ae porro
+              necessitatibus officia doloribus vero sint molestias accusantium
+              aliquid ipsa tempore maxime assumenda. Dolorum eius quas rem
+              voluptates optio voluptate. Voluptate ut nesciunt veritatis eaque
+              repellat, dolorum rerum officiis ducimus, ipsum assumenda
+              voluptatibus neque quos. Quidem optio veniam nisi dolor fuga,
+              asperiores non praesentium tempore delectus, pariatur dolorem
+              necessitatibus ab. Optio tempore ipsa nulla beatae cumque ad dicta
+              nemo culpa! Iusto consequuntur in sed dicta repellat doloribus
+              placeat perspiciatis tempore, praesentium ratione natus quo
+              consequatur officia, dolorem assumenda vitae necessitatibus!
+            </p>`
+});
